c9fa15e90b6c2326d3f90cefcd92f9a5c1974b86
Andrea Vannucchi <andrea.vannucchi@sinerbit.com>
2026-03-05 12:08:42 +0100

    Fix percorso

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
-const CACHE_NAME = 'turni-v6';
+const CACHE_NAME = 'turni-v7';
 const ASSETS = [
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json'
+  '/weekly-shifts/',
+  '/weekly-shifts/index.html',
+  '/weekly-shifts/style.css',
+  '/weekly-shifts/app.js',
+  '/weekly-shifts/manifest.json',
+  '/weekly-shifts/icon-192.png',
+  '/weekly-shifts/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
